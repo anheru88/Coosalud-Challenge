dd7a424933f84b8b4c634e1dc6965438bd9d20de
Angel Jimenez <ajimenez@digitalcaribe.com.co>
2023-09-15 13:03:33 -0500

    Add the funtionality of the pivot table

--- be/app/Models/Service.php
+++ be/app/Models/Service.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Service extends Model
 {
     use HasFactory;
 
     protected $fillable = ['code', 'name', 'coverage'];
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
